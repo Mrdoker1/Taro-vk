8332ee0ef5669a4051a44f8997a041ce922b4784
Viachas Kul <fixrapdoK@gmail.com>
2025-07-20 20:30:26 +0300

    Added logo and updated footer

--- taro-app/src/components/Footer.tsx
+++ taro-app/src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { Div, Button } from '@vkontakte/vkui';
+import logoSvg from '../assets/logo.svg';
 
 interface FooterProps {
   onAboutApp?: () => void;
@@ -13,41 +14,24 @@ export const Footer: FC<FooterProps> = ({ onAboutApp, onLegalInfo }) => {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'space-between',
-        padding: '16px',
-        borderTop: '1px solid var(--vkui--color_separator_primary)',
-        marginTop: 'auto',
-        backgroundColor: 'var(--vkui--color_background_content)'
+        padding: '12px 16px',
+        height: '44px'
       }}
     >
-      <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-        {/* Временный SVG логотип */}
-        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect width="32" height="32" rx="8" fill="var(--vkui--color_accent_blue)"/>
-          <path 
-            d="M8 12L16 8L24 12L16 16L8 12Z" 
-            fill="white"
-          />
-          <path 
-            d="M8 16L16 12L24 16L16 20L8 16Z" 
-            fill="white" 
-            opacity="0.7"
-          />
-          <path 
-            d="M8 20L16 16L24 20L16 24L8 20Z" 
-            fill="white" 
-            opacity="0.5"
-          />
-        </svg>
-        <span style={{ 
-          fontSize: '14px', 
-          fontWeight: '500',
-          color: 'var(--vkui--color_text_primary)'
-        }}>
-          Taro VK
-        </span>
+      {/* Логотип слева */}
+      <div style={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
+        <img 
+          src={logoSvg} 
+          alt="Taro VK Logo" 
+          style={{ 
+            width: '115px', 
+            height: 'auto' 
+          }} 
+        />
       </div>
       
-      <div style={{ display: 'flex', alignItems: 'center', gap: '16px' }}>
+      {/* Кнопки справа */}
+      <div style={{ display: 'flex', alignItems: 'center', gap: '24px' }}>
         <Button
           mode="tertiary"
           size="s"
